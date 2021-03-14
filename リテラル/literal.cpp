9d62f1d33227a05c00e9d0b4ef0c4d44271b7048
vbitbot08masa26 <hiroike0826masaya2020@gmail.com>
2021-03-14 21:23:40 +0900

    ver.2021.03.14.21.23

---
+++ リテラル/literal.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main() {
+	26;		//	10êiêî
+	032;		//	8êiêî
+	0x1a;		//	16êiêî
+
+	26u;		// unsignedå^ÇÃ26
+	26l;		// longå^ÇÃ26
+	26ul;		// unsigned longå^ÇÃ26
+	0x1Aul;	// unsigned longå^ÇÃ0x1A
+}
